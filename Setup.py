b1ffb3c640df9fce330f49f12317007aa66706ff
Ishimwe fabrice <110179513+sima456@users.noreply.github.com>
2023-03-27 18:51:17 +0200

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vt-prefetch-analyzer',
+    version='0.1.0',
+    install_requires=["windowsprefetch", "vt-py"]
+)
+
+#for Prefetch files 
